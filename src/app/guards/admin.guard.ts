3a0d994ff239e055b928027c086428dec4f0bfc0
Matias Campopiano <maticampopiano@hotmail.com>
2025-06-27 14:08:33 -0300

    guard especifico para panel de admin

--- src/app/guards/admin.guard.ts
+++ src/app/guards/admin.guard.ts
@@ -1,35 +1,39 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { Auth, onAuthStateChanged } from '@angular/fire/auth';
-import { AlertController } from '@ionic/angular';
 import { AuthenticationService } from '../services/authentication.service';
+import { AlertController } from '@ionic/angular';
+
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AdminGuard implements CanActivate {
   constructor(
     private auth: Auth,
+    private authService: AuthenticationService,
     private router: Router,
-    private alertController: AlertController,
-    private authService: AuthenticationService
+    private alertController: AlertController
   ) {}
 
   async canActivate(): Promise<boolean> {
     const user = await this.getCurrentUser();
 
     if (user) {
-      return true;
+      const role = await this.authService.getUserRole(user.uid);
+      if (role === 'admin') {
+        return true;
+      } else {
+        await this.presentAlert('Acceso denegado', 'Debes ser administrador para acceder.');
+        this.router.navigate(['/home']);
+        return false;
+      }
     } else {
-      await this.presentAlert();
+      await this.presentAlert('Autenticación requerida', 'Necesitas iniciar sesión.');
       this.router.navigate(['/login']);
       return false;
     }
   }
 
-  /**
-   * @function getCurrentUser
-   * @description Retorna el usuario autenticado actual.
-   */
   private getCurrentUser(): Promise<any> {
     return new Promise((resolve, reject) => {
       const unsubscribe = onAuthStateChanged(this.auth, user => {
@@ -39,18 +43,12 @@ export class AuthGuard implements CanActivate {
     });
   }
 
-
-  /**
-   * @function presentAlert
-   * @description Muestra una alerta si el usuario no está autenticado.
-   */
-  async presentAlert() {
+  private async presentAlert(header: string, message: string) {
     const alert = await this.alertController.create({
-      header: 'Autenticación requerida',
-      message: 'Necesitas iniciar sesión para acceder a esta función.',
+      header,
+      message,
       buttons: ['OK']
     });
-
     await alert.present();
   }
 }
